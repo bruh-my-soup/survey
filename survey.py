04f72b31ec9c9a782e4049793b20a002f18c5f76
bruh-my-soup <98700984+bruh-my-soup@users.noreply.github.com>
2022-04-12 20:06:11 -0500

    Add files via upload

---
+++ survey.py
@@ -0,0 +1,18 @@
+'''
+userResponse = input(text_to_user)
+'''
+
+name = input("What is your name?")
+print(name)
+
+favColor = input("What is your favorite color?")
+print(favColor)
+
+city = input("What city did you grow up in?")
+print(city)
+
+friend = input("What is your best friend's name?")
+print(friend)
+
+hair = input("What color is your hair?")
+print(hair)
